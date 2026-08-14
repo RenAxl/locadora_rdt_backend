8fe9d7424803aa64cf7746fc951830ce03a02aaa
Renan Duarte Gomes <renandg.axl@gmail.com>
2026-08-14 11:11:56 -0300

    project created

---
+++ src/main/java/com/locadora_rdt_backend/LocadoraRdtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.locadora_rdt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraRdtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraRdtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locadora_rdt_backend/LocadoraRdtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locadora_rdt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraRdtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
